75ae474d4607affe7a05c0e3a4e7381d5f8cf6fd
lnix1 <louis.nix2@gmail.com>
2025-12-11 08:53:32 -0800

    refactoring file structure to something cleaner along course lines

--- main.go
+++ main.go
@@ -1,69 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"log"
 	"github.com/lnix1/gator/internal/config"
 )
 
 type state struct {
-	config 	*config.Config
-}
-
-type command struct {
-	name	string
-	args 	[]string
-}
-
-type commands struct {
-	commandMap 	map[string]func(*state, command) error
-}
-
-func (c commands) run(s *state, cmd command) error {
-	commandFunc, ok := c.commandMap[cmd.name]
-	if !ok {
-		return fmt.Errorf("Command does not exist")
-	}
-
-	err := commandFunc(s, cmd)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (c commands) register(name string, f func(*state, command)	error) {
-	c.commandMap[name] = f
-}
-
-func handleLogin(s *state, cmd command) error {
-	if len(cmd.args) == 0 {
-		log.Fatal("Login command expects a single arg, username \n")
-	}
-
-	err := s.config.SetUser(cmd.args[0])
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("User has been set")
-	return nil
+	cfg *config.Config
 }
 
 func main() {
-	currentConfig, _ := config.Read()
-	currentState := state{config: &currentConfig}
-	currCommands := commands{commandMap: map[string]func(*state, command) error {}}
-	currCommands.register("login", handleLogin)
-	userArgs := os.Args[1:]
-	if len(userArgs) < 1 {
-		log.Fatal("not enough arguments passed")
-	}
-	currCommand := command{name: userArgs[0], args: userArgs[1:]}
-	err := currCommands.run(&currentState, currCommand)
+	cfg, err := config.Read()
 	if err != nil {
-		fmt.Printf("Got an error running command: %v", err)
+		log.Fatalf("error reading config: %v", err)
+	}
+
+	programState := &state{
+		cfg: &cfg,
+	}
+
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
+	if err != nil {
+		log.Fatal(err)
 	}
 }
